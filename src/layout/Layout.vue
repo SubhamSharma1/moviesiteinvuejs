7e71c11d1ca556953e442dfe3ed4876ccdefa8d3
Subham Sharma <ssuvam670@gmail.com>
2023-07-15 22:57:14 +0545

    Template Refactored

---
+++ src/layout/Layout.vue
@@ -0,0 +1,23 @@
+<template>
+   
+<HeaderVue/>
+<slot/>
+<FooterVue/>  
+</template>
+
+<script>
+import FooterVue from './Footer.vue'
+import HeaderVue from './Header.vue'
+export default {
+components:{
+HeaderVue,
+FooterVue
+
+
+}
+}
+</script>
+
+<style>
+
+</style>
